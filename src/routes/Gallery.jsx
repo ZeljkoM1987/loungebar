c89006e0a03040a1a4161a98f4cbf2e5098a33ad
Željko Milešević <Zeljko.Milesevic@in.mtel.ba>
2024-03-11 19:19:14 +0100

    11033

--- src/routes/Gallery.jsx
+++ src/routes/Gallery.jsx
@@ -1,24 +1,35 @@
 import { useEffect } from "react";
-
-import { GalleryItems } from "../components/GalleryItems";
-import { TripData } from "../components/TripData";
+import React, { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import { HeroOther } from "../components/HeroOther";
+import AboutImg from '../../assets/26.jpg';
+import { Images } from "../components/Images.jsx";
+import { slides } from "../components/data.jsx"
 
 export const Gallery = () => {
     useEffect(()=>{
         window.scrollTo(0, 0);
       },[])
-  return (
-    <>
-      <div className="gallery-container">
-        {GalleryItems.map((item, key) => (
-          <TripData
-            key={key}
-            image={item.url}
-            heading={item.title}
-            text={item.text}
+      return (
+        <>
+    
+    
+          <HeroOther
+            heroImg={AboutImg}
+            title="galerija" />
+    
+          
+    
+        
+    
+          <Images data={slides} onClick={(currentIndex) => { setIndex(currentIndex)}} />
+          <Lightbox
+          index={index}
+            open={index>=0}
+            close={() => setIndex(-1)}
+            slides={slides}
           />
-        ))}
-      </div>
-    </>
-  );
-};
+        </>
+      );
+    } 
